3a1dde69249f06352e47d5d7a7c073a9b420de57
liyuesolo <ex_liyue@163.com>
2016-06-22 16:44:47 +0800

    TestMainWithDiscount_Liyue

--- TestMain.java
+++ TestMain.java
@@ -22,17 +22,19 @@ public class TestMain
         x=input.nextInt();
         while(1<=x&&x<=3)
         {
-            if(x==1) {
+            if(x==1)
+            {
                 Item cokeCola = new Item("ITEM000000", "可口可乐", "瓶", 3.00);
                 shoppingChart.add(cokeCola);
             }
             if(x==2) {
-                Item cokeCola = new Item("ITEM000001", "雪碧", "瓶", 3.00);
-                shoppingChart.add(cokeCola);
+                Item sprite = new Item("ITEM000001", "雪碧", "瓶", 3.00);
+                shoppingChart.add(sprite);
             }
             if(x==3) {
-                Item cokeCola = new Item("ITEM000004", "电池", "个", 2.00);
-                shoppingChart.add(cokeCola);
+               // Item cokeCola = new Item("ITEM000004", "电池", "个", 2.00);
+                Item battery = new Item("ITEM000004", "电池", "个", 2.00,0.8);
+                shoppingChart.add(battery);
             }
             System.out.println("\n \n \n");
             test.displayInformation();
@@ -50,7 +52,8 @@ public class TestMain
     public void displayInformation(){
         System.out.println("1.ITEM000000 可口可乐  3.00/瓶");
         System.out.println("2.ITEM000001 雪碧      3.00/瓶");
-        System.out.println("3.ITEM000004 电池      2.00/个");
+       // System.out.println("3.ITEM000004 电池      2.00/个");
+        System.out.println("3.ITEM000004 电池      2.00元/个  折扣:0.8");
         System.out.println("4.打印账单");
     }
 }
