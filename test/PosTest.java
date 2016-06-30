7e0db8ee63433f5a776377401b2675c03534cefc
zyl <6527796@qq.com>
2016-06-30 22:24:37 +0800

    需求五六基础测试

---
+++ test/PosTest.java
@@ -0,0 +1,418 @@
+import com.thoughtworks.pos.common.EmptyShoppingCartException;
+import com.thoughtworks.pos.domains.Item;
+import com.thoughtworks.pos.domains.Pos;
+import com.thoughtworks.pos.domains.ShoppingChart;
+import com.thoughtworks.pos.domains.User;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by zyl on 2016/06/26.
+ */
+public class PosTest {
+    @Test(expected = EmptyShoppingCartException.class)//没有商品
+    public void testThrowExceptionWhenNoItemsInShoppingCart() throws EmptyShoppingCartException{
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+
+        // when
+        Pos pos = new Pos();
+        pos.getShoppingList(shoppingChart);
+    }
+    @Test//只有一个商品
+    public void testGetCorrectShoppingListForSingleItem() throws Exception {
+        // given
+        Item cokeCola = new Item("ITEM000000", "可口可乐", "瓶", 3.00);
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(cokeCola);
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：可口可乐，数量：1瓶，单价：3.00(元)，小计：3.00(元)\n"
+                        + "----------------------\n"
+                        + "总计：3.00(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//有两个相同的商品
+    public void testGetCorrectShoppingListForTwoSameItems() throws Exception {
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：可口可乐，数量：2瓶，单价：3.00(元)，小计：6.00(元)\n"
+                        + "----------------------\n"
+                        + "总计：6.00(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//两个单位相同的商品
+    public void testGetCorrectShoppingListForMultipleItemsWithMultipleTypes() throws Exception{
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "雪碧", "瓶", 2.00));
+        shoppingChart.add(new Item("ITEM000001", "可口可乐", "瓶", 3.00));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：雪碧，数量：1瓶，单价：2.00(元)，小计：2.00(元)\n"
+                        + "名称：可口可乐，数量：1瓶，单价：3.00(元)，小计：3.00(元)\n"
+                        + "----------------------\n"
+                        + "总计：5.00(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//两个编号不同其余相同的商品
+    public void testGetCorrectShoppingListWhenDifferentItemHaveSameItemName() throws  Exception{
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "雪碧", "瓶", 2.00));
+        shoppingChart.add(new Item("ITEM000002", "雪碧", "瓶", 3.00));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：雪碧，数量：1瓶，单价：2.00(元)，小计：2.00(元)\n"
+                        + "名称：雪碧，数量：1瓶，单价：3.00(元)，小计：3.00(元)\n"
+                        + "----------------------\n"
+                        + "总计：5.00(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//一个打折的商品
+    public void testShouldSupportDiscountWhenHavingOneFavourableItem() throws EmptyShoppingCartException {
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "雪碧", "瓶", 2.00, 0.8));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：雪碧，数量：1瓶，单价：2.00(元)，小计：1.60(元)\n"
+                        + "----------------------\n"
+                        + "总计：1.60(元)\n"
+                        + "节省：0.40(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//测试文档中正确的数据（三个促销商品）
+    public void testGetCorrectShoppingListForThreeItemsForReq4() throws Exception {
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1,true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：可口可乐，数量：3瓶，单价：3.00(元)，小计：6.00(元)\n"
+                        + "----------------------\n"
+                        + "挥泪赠送商品:\n"
+                        + "名称：可口可乐，数量：1瓶\n"
+                        + "----------------------\n"
+                        + "总计：6.00(元)\n"
+                        + "节省：3.00(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//测试文档中正确的数据（二个促销商品）
+    public void testGetCorrectShoppingListForTwoItemsForReq4() throws Exception {
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1,true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：可口可乐，数量：2瓶，单价：3.00(元)，小计：6.00(元)\n"
+                        + "----------------------\n"
+                        + "总计：6.00(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//测试购买四个促销商品
+    public void testGetCorrectShoppingListForFourItemsForReq4() throws Exception {
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1,true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：可口可乐，数量：4瓶，单价：3.00(元)，小计：9.00(元)\n"
+                        + "----------------------\n"
+                        + "挥泪赠送商品:\n"
+                        + "名称：可口可乐，数量：1瓶\n"
+                        + "----------------------\n"
+                        + "总计：9.00(元)\n"
+                        + "节省：3.00(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//测试购买五个促销商品（顾客不要第二个赠品的情况）
+    public void testGetCorrectShoppingListForFiveItemsForReq4() throws Exception {
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1,true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：可口可乐，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n"
+                        + "----------------------\n"
+                        + "挥泪赠送商品:\n"
+                        + "名称：可口可乐，数量：1瓶\n"
+                        + "----------------------\n"
+                        + "总计：12.00(元)\n"
+                        + "节省：3.00(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//测试购买六个促销商品
+    public void testGetCorrectShoppingListForSixItemsForReq4() throws Exception {
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1,true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：可口可乐，数量：6瓶，单价：3.00(元)，小计：12.00(元)\n"
+                        + "----------------------\n"
+                        + "挥泪赠送商品:\n"
+                        + "名称：可口可乐，数量：2瓶\n"
+                        + "----------------------\n"
+                        + "总计：12.00(元)\n"
+                        + "节省：6.00(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//测试两个不同的促销商品只买一个的结果
+    public void testGetCorrectShoppingListForTwoDiffer1ItemForReq4() throws Exception {
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000001", "雪碧", "瓶", 3.00, 1, true));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：可口可乐，数量：1瓶，单价：3.00(元)，小计：3.00(元)\n"
+                        + "名称：雪碧，数量：1瓶，单价：3.00(元)，小计：3.00(元)\n"
+                        + "----------------------\n"
+                        + "总计：6.00(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//测试一个打折一个促销的结果
+    public void testGetCorrectShoppingListForTwoDiffer2ItemForReq4() throws Exception {
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000004", "电池", "个", 2.00, 0.8, false));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：可口可乐，数量：1瓶，单价：3.00(元)，小计：3.00(元)\n"
+                        + "名称：电池，数量：1个，单价：2.00(元)，小计：1.60(元)\n"
+                        + "----------------------\n"
+                        + "总计：4.60(元)\n"
+                        + "节省：0.40(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//测试只买两瓶赠送商品（规定如果清单里只有两瓶表示客户并不需要赠品）和一个打折商品
+    public void testGetCorrectShoppingListForThreeItemsTwoForSaleForReq4() throws Exception {
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000004", "电池", "个", 2.00, 0.8, false));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：可口可乐，数量：2瓶，单价：3.00(元)，小计：6.00(元)\n"
+                        + "名称：电池，数量：1个，单价：2.00(元)，小计：1.60(元)\n"
+                        + "----------------------\n"
+                        + "总计：7.60(元)\n"
+                        + "节省：0.40(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//测试两个打折商品和一个促销商品
+    public void testGetCorrectShoppingItemForReq4() throws Exception {
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000004", "电池", "个", 2.00, 0.8, false));
+        shoppingChart.add(new Item("ITEM000004", "电池", "个", 2.00, 0.8, false));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：可口可乐，数量：1瓶，单价：3.00(元)，小计：3.00(元)\n"
+                        + "名称：电池，数量：2个，单价：2.00(元)，小计：3.20(元)\n"
+                        + "----------------------\n"
+                        + "总计：6.20(元)\n"
+                        + "节省：0.80(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+    @Test//测试两个打折商品和三个促销商品
+    public void testGetCorrectShoppingItemForReq41() throws Exception {
+        // given
+        ShoppingChart shoppingChart = new ShoppingChart();
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000000", "可口可乐", "瓶", 3.00, 1, true));
+        shoppingChart.add(new Item("ITEM000004", "电池", "个", 2.00, 0.8, false));
+        shoppingChart.add(new Item("ITEM000004", "电池", "个", 2.00, 0.8, false));
+
+        // when
+        Pos pos = new Pos();
+        String actualShoppingList = pos.getShoppingList(shoppingChart);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        // then
+        String expectedShoppingList =
+                "***商店购物清单***\n"
+                        +"打印时间："
+                        +new String(dateFormat.format(new Date()))+"\n"
+                        +"----------------------\n"
+                        + "名称：可口可乐，数量：3瓶，单价：3.00(元)，小计：6.00(元)\n"
+                        + "名称：电池，数量：2个，单价：2.00(元)，小计：3.20(元)\n"
+                        + "----------------------\n"
+                        + "挥泪赠送商品:\n"
+                        + "名称：可口可乐，数量：1瓶\n"
+                        + "----------------------\n"
+                        + "总计：9.20(元)\n"
+                        + "节省：3.80(元)\n"
+                        + "**********************\n";
+        assertThat(actualShoppingList, is(expectedShoppingList));
+    }
+
+}
